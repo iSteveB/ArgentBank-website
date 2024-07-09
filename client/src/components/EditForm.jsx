38e7837c059895192effd0bec64c13793711b45f
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-07-09 13:58:37 +0200

    Add style to edit form

--- client/src/components/EditForm.jsx
+++ client/src/components/EditForm.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { getUser } from '../feature/userSlice';
 import { updateUser } from '../feature/updateUserSlice';
 
 const EditForm = ({ setShowEditForm }) => {
-  const token = useSelector((state) => state?.auth?.token);
+	const token = useSelector((state) => state?.auth?.token);
 	const user = useSelector((state) => state?.user?.user);
 	const dispatch = useDispatch();
 	const [updateUsername, setUpdateUsername] = useState(user?.userName);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-    dispatch(updateUser({ token, userName: updateUsername }));
-    setShowEditForm(false);
+		dispatch(updateUser({ token, userName: updateUsername }));
+		dispatch(getUser(token));
+		setShowEditForm(false);
 	};
 
 	return (
 		<div>
 			<h1>Edit user info</h1>
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={handleSubmit} className='edit-form'>
 				<div>
 					<label>User name:</label>
 					<input
@@ -36,10 +38,17 @@ const EditForm = ({ setShowEditForm }) => {
 					<input type='text' value={user?.lastName} disabled />
 				</div>
 
-				<button type='submit' className='edit-button'>Save</button>
-				<button type='button' className='edit-button' onClick={() => setShowEditForm(false)}>
-					Cancel
-				</button>
+				<div>
+					<button type='submit' className='edit-button'>
+						Save
+					</button>
+					<button
+						type='button'
+						className='edit-button'
+						onClick={() => setShowEditForm(false)}>
+						Cancel
+					</button>
+				</div>
 			</form>
 		</div>
 	);
